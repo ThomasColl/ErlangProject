ff78de9cb9935a5989aa85ae1e8959bcb2fb6655
Tom <trlc1996@hotmail.com>
2019-03-14 15:37:42 +0000

    Added parser, attempting RPN

--- src/project.erl
+++ src/project.erl
@@ -10,16 +10,50 @@
 -author("Thomas Coll").
 
 %% API
--export([tokeniser/0]).
+-export([start/0]).
 
-tokeniser() ->
-  Input = io:fread('Please input your mathmatic equation: ', "~d"),
-  parser(Input).
+start() ->
+  Input = "((2+3)-1)",
+%%  {ok, [Input]} = io:fread('Please input your mathmatic equation: ', "~s"),
+  evaluator(parse(tokenise(Input), [])).
 
-parser(Input) ->
-  HandledInput = [],
-  evaluator(HandledInput).
+tokenise([$(|Tail]) ->
+  [{s, lb}|tokenise(Tail)];
+tokenise([$)|Tail]) ->
+  [{s, rb}|tokenise(Tail)];
+tokenise([$+|Tail]) ->
+  [{o, plus}|tokenise(Tail)];
+tokenise([$-|Tail]) ->
+  [{o, minus}|tokenise(Tail)];
+tokenise([$*|Tail]) ->
+  [{o, multiply}|tokenise(Tail)];
+tokenise([$/|Tail]) ->
+  [{o, divide}|tokenise(Tail)];
+tokenise([Head|Tail]) ->
+  [{n, Head}|tokenise(Tail)];
+tokenise([]) -> [].
 
+parse([{$n, Char}|Stack], RPN)->
+  parse(Stack, [Char|RPN]);
+parse([{$o, Char}|Stack], RPN) ->
+    Rhs = hd(RPN),
+    Lhs = hd(tl(RPN)),
+    Res = apply(op(Char), [Lhs, Rhs]),
+    parse(Stack, [Res|tl(tl(RPN))]);
+parse([], RPN) -> RPN.
+%%  parser(TokenisedList, []).
+
+op("+") -> fun(L,R) -> L+R end;
+op("*") -> fun(L,R) -> L*R end.
+
+is_op(X) -> X == "+" orelse X == "*".
+
+
+%%parser([{Symbol, Value}|Tail], Evaluation) ->
+%%  parser(Tail, Evaluation);
+%%parser(Evaluation) ->
+%%  evaluator(Evaluation).
+%%
 evaluator(ParsedInput) ->
   io:format('After careful and ingenius consideration I have concluded:\n'),
   io:fwrite("~w",[ParsedInput]).
