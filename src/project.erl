8082cd9eb7c481e50fac87cbfc7c5768acc64704
Tom <trlc1996@hotmail.com>
2019-03-14 16:26:02 +0000

    Improve RPN

--- src/project.erl
+++ src/project.erl
@@ -15,12 +15,12 @@
 start() ->
   Input = "((2+3)-1)",
 %%  {ok, [Input]} = io:fread('Please input your mathmatic equation: ', "~s"),
-  evaluator(parse(tokenise(Input), [])).
+  evaluator(parse(tokenise(Input), [], [])).
 
 tokenise([$(|Tail]) ->
-  [{s, lb}|tokenise(Tail)];
+  [{s, "l"}|tokenise(Tail)];
 tokenise([$)|Tail]) ->
-  [{s, rb}|tokenise(Tail)];
+  [{s, "r"}|tokenise(Tail)];
 tokenise([$+|Tail]) ->
   [{o, plus}|tokenise(Tail)];
 tokenise([$-|Tail]) ->
@@ -33,20 +33,37 @@ tokenise([Head|Tail]) ->
   [{n, Head}|tokenise(Tail)];
 tokenise([]) -> [].
 
-parse([{$n, Char}|Stack], RPN)->
-  parse(Stack, [Char|RPN]);
-parse([{$o, Char}|Stack], RPN) ->
-    Rhs = hd(RPN),
-    Lhs = hd(tl(RPN)),
-    Res = apply(op(Char), [Lhs, Rhs]),
-    parse(Stack, [Res|tl(tl(RPN))]);
-parse([], RPN) -> RPN.
-%%  parser(TokenisedList, []).
+parse([{$n, Value} | Remain], Stack, RPN) ->
+  parse(Remain, Stack, [Value|RPN]);
+parse([{$o, Value} | Remain], Stack, RPN) ->
+  parse(Remain, [Value|Stack], RPN);
+parse([{$s, Value} | Remain], Stack, RPN) ->
+  parse(Remain, [Value|Stack], RPN);
+parse([{$s, $r} | Remain], [Head|Tail], RPN) ->
+  parse(Remain, pop(Tail), [Head|RPN]);
+parse([], [Head|Tail], RPN) ->
+  parse([], Tail, [Head|RPN]);
+parse([], [], RPN) ->
+  evaluator(RPN).
 
-op("+") -> fun(L,R) -> L+R end;
-op("*") -> fun(L,R) -> L*R end.
-
-is_op(X) -> X == "+" orelse X == "*".
+pop([_|Tail]) ->
+  pop(Tail);
+pop([$l|Tail]) ->
+  [Tail].
+%%parse([{$n, Char}|Stack], RPN)->
+%%  parse(Stack, [Char|RPN]);
+%%parse([{$o, Char}|Stack], RPN) ->
+%%    Rhs = hd(RPN),
+%%    Lhs = hd(tl(RPN)),
+%%    Res = apply(op(Char), [Lhs, Rhs]),
+%%    parse(Stack, [Res|tl(tl(RPN))]);
+%%parse([], RPN) -> RPN.
+%%%%  parser(TokenisedList, []).
+%%
+%%op("+") -> fun(L,R) -> L+R end;
+%%op("*") -> fun(L,R) -> L*R end.
+%%
+%%is_op(X) -> X == "+" orelse X == "*".
 
 
 %%parser([{Symbol, Value}|Tail], Evaluation) ->
